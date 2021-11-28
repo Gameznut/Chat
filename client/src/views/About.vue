56fa9384cb157a2d7ff50f83744a785c3688e723
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2021-11-28 13:23:06 +0100

    yes

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Chatapp msg="1"/>
   </div>
 </template>
+<script>
+import Chatapp from '../components/Chatapp.vue'
+export default {
+  components: { Chatapp },
+}
+</script>
