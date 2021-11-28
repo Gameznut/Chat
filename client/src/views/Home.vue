683f385580fb65688e5b6a984931ec6162dab461
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2021-11-28 13:21:31 +0100

    ch

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="w-3/4">
+      <canvas
+        ref="game"
+        width="640"
+        height="240"
+        class="border w-full border-black"
+      ></canvas>
+      <div class="flex gap-2 justify-between my-2">
+        <button
+          class="bg-green-400 hover:bg-green-600 w-full"
+          @click="move('left')"
+          @keyup.left="move()"
+        >
+          Left
+        </button>
+        <div class="w-full gap-2 flex flex-col">
+          <button
+            class="bg-green-400 w-full p-3"
+            @click="move('up')"
+            @keyup.up="position.y += 5"
+          >
+            Up
+          </button>
+          <button
+            class="bg-green-400 w-full p-3"
+            @click="move('down')"
+            @keyup.down="move()"
+          >
+            Down
+          </button>
+        </div>
+
+        <button
+          class="bg-green-400 w-full p-2"
+          @click="move('right')"
+          @keyup.right="move()"
+        >
+          Right
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import io from "socket.io-client";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      socket: {},
+      context: {},
+      position: {
+        x: 0,
+        y: 0,
+      },
+    };
+  },
+  created() {
+    this.socket = io("http://localhost:8000", {
+      withCredentials: true,
+      extraHeaders: {
+        "my-custom-header": "abcd",
+      },
+    });
+  },
+  mounted() {
+    this.context = this.$refs.game.getContext("2d");
+    this.socket.on("position", (result) => {
+      this.position = result;
+      this.context.clearRect(
+        0,
+        0,
+        this.$refs.game.width,
+        this.$refs.game.height
+      );
+      this.context.fillRect(this.position.x, this.position.y, 50, 20);
+    });
+  },
+  methods: {
+    move(direction) {
+      this.socket.emit("move", direction);
+    },
+    moveUp() {
+      this.position.y += 5;
+    },
   },
 };
 </script>
---
+++ server/server.js
@@ -0,0 +1,79 @@
+const express = require('express')()
+const http = require('http').Server(express)
+const socketio = require('socket.io')(http,{
+    cors: {
+      origin: "http://localhost:8080",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  })
+const port = 8000
+const volleyball = require('volleyball');
+
+var position = {
+    x: 200,
+    y: 110
+}
+express.use(volleyball)
+
+express.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+socketio.on("connection", socket => {
+    socket.emit("position", position)
+    console.log("there is connection");
+    socket.on("move", data =>{
+        switch(data){
+            case"left":
+                position.x -= 5
+                socketio.emit("position", position)
+                break
+            case"right":
+                position.x += 5
+                socketio.emit("position", position)
+                break
+            case"up":
+                position.y -= 5
+                socketio.emit("position", position)
+                break
+            case"down":
+                position.y += 5
+                socketio.emit("position", position)
+                break
+        }
+    })
+})
+   
+http.listen(port, () => {
+  console.log(`listening on port: ${port}........`);
+});
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+// const http = require('http')
+// const server = http.createServer(app)
+// const socketio = require('socket.io')(http)
+
+//app.use(express.static('../client/public'))
+
+/*app.use(volleyball)
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(port, () => {
+    console.log(`listening on port: ${port}........`);
+  });
+*/
